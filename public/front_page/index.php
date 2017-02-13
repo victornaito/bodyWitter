64721ff3f2d2bc1b9bb8b2e532ea29baf3a4aebc
Lucas Santana <cience@Lucass-MacBook-Pro.local>
2017-02-13 16:34:20 -0300

    Pagina inicial com login

---
+++ public/front_page/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/front.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+    <title></title>
+  </head>
+  <body>
+
+    <!-- Container Principal-->
+    <nav class="container">
+        <p class="navbar-text">Bodywitter <img src="images/logo.png" alt=""></p>
+    </nav><!-- Navbar -->
+
+    <!-- Container -->
+    <div class="container">
+      <div class="row">
+        <div class="row">
+          <div class="col-sm-1">
+          </div>
+      </div>
+    </div><!-- Container -->
+
+    <!-- Container -->
+    <div class="container">
+      <div class="row">
+
+          <!-- Letreiro -->
+          <div class="col-sm-7">
+            <br></br>
+            <h1 class="page-name size-large">Bodywitter</h1>
+            <h3> Mais <span class="especial-font">importante</span> que a vontade de <span class="especial-font">vencer</span> é a
+                 coragem de <span class="especial-font">começar</span>
+            </h3>
+          </div><!-- Letreiro -->
+
+          <!-- Espaco em branco  -->
+          <div class="col-sm-2">
+          </div><!-- Espaco em branco  -->
+
+        <!-- Login / Logon -->
+        <br></br>
+        <div class="login-card">
+          <h1 class="black-color page-name">Entrar</h1><br>
+        <form>
+          <input type="text" name="user" placeholder="Usuário">
+          <input type="password" name="pass" placeholder="Senha">
+          <input type="submit" name="login" class="login login-submit but-color" value="login">
+        </form>
+
+        <div class="login-help">
+          <a href="#">Cadastrar</a> • <a href="#">Equeci a senha</a>
+        </div>
+        </div>
+        <!-- Login / Logon -->
+
+
+        </div>
+      </div>
+    </div><!-- Container Principal -->
+
+
+  </body>
+</html>
